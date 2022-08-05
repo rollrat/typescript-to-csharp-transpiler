dd5e41a03e95e24610f129b6101abd91505ef25e
rollrat <rollrat.cse@gmail.com>
2022-08-05 10:50:53 +0900

    test babel typescript parser

--- app.ts
+++ app.ts
@@ -1,2 +1,21 @@
+import * as babel from "@babel/core";
 
-console.log("asdf")
+const s: number = 0;
+
+const sourceCode = "const s: number = 0";
+const parsedAst = babel.parseSync(sourceCode, {
+  parserOpts: {
+    allowReturnOutsideFunction: true,
+    plugins: ["typescript"],
+  },
+});
+
+const options = { ast: true } as babel.TransformOptions;
+
+const { code, map, ast } = babel.transformFromAstSync(
+  parsedAst!,
+  sourceCode,
+  options!
+)!;
+
+console.log(ast);
