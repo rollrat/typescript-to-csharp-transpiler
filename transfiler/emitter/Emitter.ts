10f073184a45b7f91d0e4cee44df984a99dc79be
rollrat <rollrat.cse@gmail.com>
2022-08-22 22:51:42 +0900

    feat(emitter): Implements space management routine

--- transfiler/emitter/Emitter.ts
+++ transfiler/emitter/Emitter.ts
@@ -1,7 +1,26 @@
 export default class CSEmitter {
   code: string = "";
+  spaceCount: number = 0;
+
+  lastSpaceCount: number = 0;
+  spaceStr: string = "";
+
+  private ensureSpace() {
+    if (this.spaceCount === this.lastSpaceCount) return;
+
+    this.spaceStr = " ".repeat(4 * this.spaceCount);
+  }
 
   println(snippet: string) {
-    this.code += `${snippet}\n`;
+    this.ensureSpace();
+    this.code += `${this.spaceStr}${snippet}\n`;
+  }
+
+  enterScope() {
+    this.spaceCount += 4;
+  }
+
+  exitScope() {
+    this.spaceCount -= 4;
   }
 }
